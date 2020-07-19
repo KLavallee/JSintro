cf003024267a1d83505d16b91181269a07e041f4
klavallee <lava.k@protonmail.com>
2020-07-18 23:47:51 -0400

    finish higher order fncs

---
+++ setInterval.js
@@ -0,0 +1,13 @@
+function sing() {
+    console.log("Twinkle twinkle...");
+    console.log("how i wonder...");
+}
+
+// clearInterval(num it gives)
+setInterval(sing , 1000)
+
+// another way
+// setInterval(funtion(){
+   // console.log(" i am an anonymous funtion");
+   // console.log(" this is aweseom");
+// }, 2000)
